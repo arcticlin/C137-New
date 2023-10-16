0bb08dd7d0c994f610f31a487792297335419793
arcticlin <zarcticz@gmail.com>
2023-10-16 10:47:25 +0800

    WebSocket模块

--- app/router/ws/ws.py
+++ app/router/ws/ws.py
@@ -13,6 +13,7 @@ from starlette.websockets import WebSocketDisconnect
 from app.handler.token_handler import UserToken
 from app.services.ws.client_store import connected_clients
 from app.services.ws.ws_service import WsService
+from loguru import logger
 
 ws = APIRouter()
 
@@ -24,15 +25,16 @@ async def websocket_endpoint(websocket: WebSocket, token: str = Query(...)):
         UserToken.parse_token(token)
         user_id = UserToken.get_user_id_from_token(token)
         await websocket.accept()
-        if websocket not in connected_clients:
-            await websocket.send_json(WsService.success_connect())
-        connected_clients[user_id] = websocket
+        if user_id not in connected_clients or connected_clients[user_id] != websocket:
+            connected_clients[user_id] = websocket
+        await websocket.send_json(WsService.connect_success())
         while True:
             data = await websocket.receive_text()
             for client in connected_clients.keys():
                 await connected_clients[client].send_text(data)
     except WebSocketDisconnect as e:
-        pass
+        logger.debug(f"client: {user_id} disconnect")
+        await WsService.remove_client(user_id)
     except Exception as e:
         print("11hh", e)
         pass
